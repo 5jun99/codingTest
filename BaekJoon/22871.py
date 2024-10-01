e07a2f8ae1ece5d5725dd20d81782295c8697ffb
5jun99 <oj041088@gmail.com>
2024-10-01 02:00:00 +0000

    09/29-1

--- BaekJoon/22871.py
+++ BaekJoon/22871.py
@@ -1,9 +1,14 @@
 import sys
 input = sys.stdin.readline
-
+INF = 999999999
 N = int(input())
-A = list(map(int, input().split))
-k = 0
-index = 0
+A = list(map(int, input().split()))
 
+dp = [0] + [INF] * (N - 1)
 
+for i in range(1, N):
+    for j in range(i):
+        k = max((i - j) * (1 + abs(A[i] - A[j])), dp[j])
+        dp[i] = min(dp[i], k)
+
+print(dp[-1])
