a4f56dacb42dd0edf50f5b1ecb087e8f9f91d839
5jun99 <oj041088@gmail.com>
2024-10-08 18:14:12 +0900

    10/08-1

---
+++ study/2.py
@@ -0,0 +1,12 @@
+dp = [0] * 251
+dp[0] = 1
+dp[1] = 1
+dp[2] = 3
+while True:
+    try:
+        n = int(input())
+        for i in range(3, n + 1):
+            dp[i] = dp[i-1] + dp[i-2] * 2
+        print(dp[n])
+    except:
+        break
