5bb9fd2151871ba02ca90f23110be0921b994b94
5jun99 <oj041088@gmail.com>
2025-06-08 23:23:22 +0000

    06/08

--- shinhan/12891.py
+++ shinhan/12891.py
@@ -2,8 +2,24 @@ from collections import defaultdict
 
 s, p = map(int, input().split())
 dna_count = defaultdict(int)
-
 dna = input()
-acgt = list(map(int, input().split()))
+a, c, g, t = map(int, input().split())
 
 
+answer = 0
+left = 0
+right = p - 1
+for d in dna[left:right]:
+    dna_count[d] += 1
+
+while right < s:
+    dna_count[dna[right]] += 1
+
+    if dna_count['A'] >= a and dna_count['C'] >= c and dna_count['G'] >= g and dna_count['T'] >= t:
+        answer += 1
+
+    dna_count[dna[left]] -= 1
+    left += 1
+    right += 1
+
+print(answer)
